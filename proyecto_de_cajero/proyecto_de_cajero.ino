190fa85e7f09d8635660b898f30d48d01259449d
Alexis Diaz Huaringa <98545993+Alexishun@users.noreply.github.com>
2022-04-12 01:00:11 -0500

    codigo en wokwi

--- proyecto_de_cajero/proyecto_de_cajero.ino
+++ proyecto_de_cajero/proyecto_de_cajero.ino
@@ -13,7 +13,7 @@ char keys[FILAS][COLUMNAS] = {    // define la distribucion de teclas
   {'7','8','9','C'},
   {'*','0','.','D'}
 };
-byte pinesFilas[FILAS] = {9,8,7,6};   // pines correspondientes a las filas
+byte pinesFilas[FILAS] = {12,8,7,6};   // pines correspondientes a las filas
 byte pinesColumnas[COLUMNAS] = {5,4,3,2}; // pines correspondientes a las columnas
 
 Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS);  // crea objeto
@@ -24,16 +24,15 @@ float valor=0;
 float monto=0;   
 
 
-int TRIG = 10;
-int ECO = 9;
+int TRIG = A1;
+int ECO = A0;
 int DURACION;
-int DISTANCIA;
+float DISTANCIA;
 
 //Monedas
 int num_10;
 int num_50;
 int num_100;
-int monto;
 int DuracionEntreMonedas = 1000;
 int recibe_10;
 int recibe_50;
@@ -45,7 +44,9 @@ Servo servo1; //100
 Servo servo2; //50
 Servo servo3; //10
 
-int PINSERVO = 2;
+int PINSERVO_1 = 9;
+int PINSERVO_2 = 10;
+int PINSERVO_3 = 11;
 int PULSOMIN = 1000;
 int PULSOMAX = 2000;
 
@@ -67,118 +68,129 @@ void setup() {
   pinMode(TRIG, OUTPUT);
   pinMode(ECO, INPUT);
   
-  servo1.attach(PINSERVO, PULSOMIN, PULSOMAX);
-  servo2.attach(PINSERVO, PULSOMIN, PULSOMAX);
-  servo3.attach(PINSERVO, PULSOMIN, PULSOMAX);
+  servo1.attach(PINSERVO_1, PULSOMIN, PULSOMAX);
+  servo2.attach(PINSERVO_2, PULSOMIN, PULSOMAX);
+  servo3.attach(PINSERVO_3, PULSOMIN, PULSOMAX);
 }
 
 void loop() {
 
   //ENTRAN LAS MONEDAS
   digitalWrite(TRIG, HIGH); 
-  delay(1);	
-  digitalWrite(TRIG, LOW);	
+  delay(1);  
+  digitalWrite(TRIG, LOW);  
   
   DURACION = pulseIn(ECO, HIGH);
   
   DISTANCIA = DURACION / 58.2;
-  Serial.println(DISTANCIA);
-  if(0<DISTANCIA && DISTANCIA<60){
-    num_100++;
-    delay(DuracionEntreMonedas);
-  }else if(60<DISTANCIA && DISTANCIA<120){
-    num_50++;
-    delay(DuracionEntreMonedas);
-  }else if(120<DISTANCIA && DISTANCIA<180){
-    num_10++;
-    delay(DuracionEntreMonedas);
-  }
-  delay(200);
-
-
-  //Cobra
-  // put your main code here, to run repeatedly:
-  while(true){
-    TECLA=teclado.getKey();
-    if (TECLA!=""){
-      cadena+=TECLA;
-      Serial.println(TECLA);
-      lcd.print(TECLA);
-      if(TECLA=='A'){
-        lcd.clear();
-        delay(100);
-        cadena="";
-        Serial.println("LLEGO HASTA A");
-        break;
-      }
-      TECLA="";
-    };
-    if (TECLA==D){
-      //funcion para calcular el numero de monedas
-      func_monedas(cadena);
-      delay(50);
-      cadena = "";
-      Serial.println(recibe_100);
-      for (int i = 0; i < recibe_100; i++)
-      {
-        if(a==180){
-          servo1.write(0);
-          delay(2000);
-          a=0;
-        }else{
-          servo1.write(180);
-          delay(2000);
-          a=180;
+  if(DISTANCIA<380){
+    if(0<DISTANCIA && DISTANCIA<100){
+      num_100++;
+      delay(DuracionEntreMonedas);
+    }else if(100<=DISTANCIA && DISTANCIA<200){
+      num_50++;
+      delay(DuracionEntreMonedas);
+    }else if(200<=DISTANCIA && DISTANCIA<=300){
+      num_10++;
+      delay(DuracionEntreMonedas);
+    }else{
+      lcd.setCursor(0,1);
+      while(TECLA=teclado.getKey()){
+        if (TECLA!='D'){
+          cadena+=TECLA;
+          Serial.println(TECLA);
         }
-      }
-      Serial.println(recibe_50);
-      for (int i = 0; i < recibe_50; i++)
-      {
-        if(b==180){
-          servo1.write(0);
-          delay(2000);
-          b=0;
-        }else{
-          servo1.write(180);
-          delay(2000);
-          b=180;
+        lcd.print(cadena);
+        if(TECLA=='A'){
+          lcd.clear();
+          cadena="";
+          Serial.println("LLEGO HASTA A");
+          break;
         }
-      }
-      Serial.println(recibe_10);
-      for (int i = 0; i < recibe_10; i++)
-      {
-        if(c==180){
-          servo2.write(0);
-          delay(2000);
-          c=0;
-        }else{
-          servo2.write(180);
-          delay(2000);
-          c=180;
+        if (TECLA=='D'){
+          //funcion para calcular el numero de monedas
+          Serial.print("Se ha presionado D");
+          func_monedas(cadena);
+          delay(50);
+          cadena = "";
+          Serial.println("Imprime un sol");
+          Serial.println(recibe_100);
+          for (int i = 0; i < recibe_100; i++)
+          {
+            if(a==180){
+              servo1.write(0);
+              delay(2000);
+              a=0;
+            }else{
+              servo1.write(180);
+              delay(2000);
+              a=180;
+            }
+          }
+          Serial.println("Imprime cincuenta");
+          Serial.println(recibe_50);
+          for (int i = 0; i < recibe_50; i++)
+          {
+            if(b==180){
+              servo2.write(0);
+              delay(2000);
+              b=0;
+            }else{
+              servo2.write(180);
+              delay(2000);
+              b=180;
+            }
+          }
+          Serial.println("Imprime diecentimom");
+          Serial.println(recibe_10);
+          for (int i = 0; i < recibe_10; i++)
+          {
+            if(c==180){
+              servo3.write(0);
+              delay(2000);
+              c=0;
+            }else{
+              servo3.write(180);
+              delay(2000);
+              c=180;
+            }
+          }
         }
       }
     }
   }
   lcd.setCursor(0,0);
-  lcd.print(cadena);
-};
-
+  monto=monto_Pantalla(num_100,num_50,num_10);
+  lcd.print(monto);
+}
+float monto_Pantalla(int unsol,int cincuenta,int diez){
+  float monto=unsol*1.0+(cincuenta*1.0)/2.0+(diez*1.0)/10.0;
+  return monto;
+}
 void func_monedas(String cadena){
-  int intCadena = toInt(cadena);
-  int parteEntera = cadena;
+  float intCadena = cadena.toFloat();
+  int parteEntera = cadena.toInt();
   //parte decimal ->partD
-  float partD = cadena - parteEntera;
-  recibe_100 = intCadena;
-  if (partD<100){
-    if(partD>50){
+  float partD = intCadena - parteEntera;
+  recibe_100 = parteEntera;
+  if (partD>0 && partD<1){
+    if(partD>0.5){
       recibe_50 = 1;
-      
-    } else{
-      recibe_10 = partD / 10; 
-    }
+      if(partD>=0.9){
+        recibe_10 = 4;
+      }else if (partD>=0.8){
+        recibe_10 = 3;
+      }else if (partD>=0.7){
+        recibe_10 = 2;
+      }else if (partD>=0.6){
+        recibe_10 = 1;
+      }
+    } 
+  else{
+      recibe_10 = partD * 10; 
+  }
   }else{
     Serial.println("Parte decimal fuera de rango");
   }
 }
-
 
